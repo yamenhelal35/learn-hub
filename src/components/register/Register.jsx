d038b4014d54b169d1c5a0ad27ac631e02cbcfd0
Ahmed Magdy <azzam9778@gmail.com>
2024-02-28 13:54:50 +0200

    Register

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -5,6 +5,7 @@ import logo from "../images/Logo.png";
 import Text from "../images/Text.svg";
 import logo2 from "../images/login-promotion 1.png";
 import github from "../images/github-svgrepo-com.svg"
+import { Link } from 'react-router-dom';
 // consts 
 const Register = () => {
 const colors = {
@@ -40,35 +41,9 @@ const colors = {
             </button>
           </div>
 
-          <div className="w-full flex items-center justify-center relative py-2">
-            <div className="w-full h-[0.5px] bg-gray-400"></div>
-            <p className="absolute bg-[#FAFAFA] text-gray-400 p-2">or</p>
-          </div>
+         
 
-          <div class="mt-7 flex flex-col gap-2 my-1">
-
-            <button
-              class="inline-flex h-10 w-full items-center justify-center gap-2 rounded border border-slate-300 bg-black p-2 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-[#968BC9] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60"><img
-                src={github} alt="GitHub"
-                class="h-[18px] w-[18px] fill-slate-50 " />
-              Continue with GitHub
-            </button>
-
-            <button
-              class="inline-flex h-10 w-full items-center justify-center gap-2 rounded border border-slate-300 bg-white p-2 text-sm font-medium text-black outline-none focus:ring-2 focus:ring-[#968BC9] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60"><img
-                src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google"
-                class="h-[18px] w-[18px] " />Continue with
-              Google
-            </button>
-
-
-            <button
-              class="inline-flex h-10 w-full items-center justify-center gap-2 rounded border border-slate-300 bg-white p-2 text-sm font-medium text-black outline-none focus:ring-2 focus:ring-[#968BC9] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60"><img
-                src="https://www.svgrepo.com/show/448234/linkedin.svg" alt="Google"
-                class="h-[18px] w-[18px] " />Continue with
-              LinkedIn
-            </button>
-          </div>
+        
         </div>
       </div>
 
@@ -76,9 +51,11 @@ const colors = {
       <div className="w-1/2 h-screen flex flex-col p-10 justify-between">
         <img src={Text} alt="" className="mb-3" />
         <img src={logo2} alt="" className="w-97"/>
+        <Link to="/">
         <button className="w-full bg-[#968BC9] rounded-md  p-4 text-center font-semibold text-white flex items-center justify-center">
-            Join Now For Free
+            Login to Existing Account
             </button>
+      </Link>
       </div>
     </div>
   );
