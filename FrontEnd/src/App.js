6a99bb3d6d4385a778942d4207cbb49f8dad857c
Ahmed Magdy <azzam9778@gmail.com>
2024-04-24 21:35:36 +0200

    Added Calendar

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,17 +4,19 @@ import Login from './pages/login/login';
 import Register from './pages/register/Register';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Landing from './components/landingPage/landing';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/Home" element={<Landing />} />
-        {/* <Landing></Landing> */}
-      </Routes>
-    </Router>
+<Dashboard/>
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/" element={<Login />} />
+    //     <Route exact path="/register" element={<Register />} />
+    //     <Route exact path="/Home" element={<Landing />} />
+    //     {/* <Landing></Landing> */}
+    //   </Routes>
+    // </Router>
   );
 }
 
