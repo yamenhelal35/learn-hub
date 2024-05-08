b5502fc3e65aa8afa9de83e5fe8ea7629b83a7eb
Ahmed Magdy <azzam9778@gmail.com>
2024-05-08 22:17:40 +0300

    Landing Page Done (Footer Null)

--- FrontEnd/src/components/landingPage/landing.jsx
+++ FrontEnd/src/components/landingPage/landing.jsx
@@ -12,6 +12,7 @@ import Sidebar from "../StickyComponent/Side Bar/Sidebar";
 
 
 
+
 // import Card from "./Card/card";
 
 
@@ -25,7 +26,7 @@ const colors = {
 function Landing() {
   return (
 
-    <div className="flex flex-col bg-[#F4F0FF]">
+    <div className="flex flex-col bg-white dark:bg-gray-900">
 
 
       {/* Side Bar Fixed */}
@@ -94,93 +95,83 @@ function Landing() {
             </div>
           </section>
         </header>
-        {/* ===================End of Hero Section =================================== */}
+        
 
-        <br /><br />
 
-        {/* Service Section */}
-        <section className="flex flex-col justify-center items-center flex-grow">
-          <h3 className="text-2xl font-semibold">OUR SERVICE</h3>
-          <img src={Service} className="w-120 justify-center" alt="Service" />
+        {/* =====================Service Parts ====================================== */}
+        <section className="Service">
+          <section class="bg-white dark:bg-gray-900 flex justify-center items-center">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+              <div class="max-w-screen-md mb-8 lg:mb-16">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+              </div>
+              <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                <div>
+                  <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                  </div>
+                  <h3 class="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
+                  <p class="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
+                </div>
+                <div>
+                  <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
+                  </div>
+                  <h3 class="mb-2 text-xl font-bold dark:text-white">Legal</h3>
+                  <p class="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
+                </div>
+                <div>
+                  <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>
+                  </div>
+                  <h3 class="mb-2 text-xl font-bold dark:text-white">Business Automation</h3>
+                  <p class="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.</p>
+                </div>
+                <div>
+                  <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd"></path></svg>
+                  </div>
+                  <h3 class="mb-2 text-xl font-bold dark:text-white">Finance</h3>
+                  <p class="text-gray-500 dark:text-gray-400">Audit-proof software built for critical financial operations like month-end close and quarterly budgeting.</p>
+                </div>
+                <div>
+                  <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
+                  </div>
+                  <h3 class="mb-2 text-xl font-bold dark:text-white">Enterprise Design</h3>
+                  <p class="text-gray-500 dark:text-gray-400">Craft beautiful, delightful experiences for both marketing and product with real cross-company collaboration.</p>
+                </div>
+                <div>
+                  <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+                  </div>
+                  <h3 class="mb-2 text-xl font-bold dark:text-white">Operations</h3>
+                  <p class="text-gray-500 dark:text-gray-400">Keep your company’s lights on with customizable, iterative, and structured workflows built for all efficient teams and individual.</p>
+                </div>
+              </div>
+            </div>
+          </section>
         </section>
-        <br /><br /><br />
-
-        <h3 className="text-2xl font-semibold justify-center items-center">WHAT MAKES US DIFFRENET?</h3>
-
-        <br /><br /><br />
 
 
-        {/* What makes us diffrent Part */}
-        <section className="flex justify-center flex-grow gap-8">
 
-          <div className="relative flex flex-col w-64 rounded-xl bg-white text-gray-700 shadow-md">
-            <div className="relative mx-4 -mt-6 h-32 overflow-hidden rounded-t-xl bg-blue-gray-500 text-white shadow-lg shadow-blue-gray-500/40">
-              <img src={Courses} />
+        {/* ========================Images Part ============================ */}
+        <section className="ImagesPart">
+          <section class="bg-white dark:bg-gray-900">
+            <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+              <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
+                <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+              </div>
+              <div class="grid grid-cols-2 gap-4 mt-8">
+                <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+              </div>
             </div>
-            <div className="p-4">
-              <h5 className="mb-2 block font-sans text-lg font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                Courses
-              </h5>
-              <p className="block font-sans text-sm font-light leading-relaxed text-inherit antialiased">
-                Acess Hundreds of Courses for free and with paid bundles.
-              </p>
-            </div>
-            <div className="p-4 pt-0">
-              <button
-                data-ripple-light="true"
-                type="button"
-                className="select-none rounded-lg bg-[#968BC9] py-2 px-4 text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              >
-                Read More
-              </button>
-            </div>
-          </div>
-          <div className="relative flex flex-col w-64 rounded-xl bg-white text-gray-700 shadow-md">
-            <div className="relative mx-4 -mt-6 h-32 overflow-hidden rounded-t-xl bg-blue-gray-500 text-white shadow-lg shadow-blue-gray-500/40">
-              <img src={Exam} />
-            </div>
-            <div className="p-4">
-              <h5 className="mb-2 block font-sans text-lg font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                Exams & Quizzes
-              </h5>
-              <p className="block font-sans text-sm font-light leading-relaxed text-inherit antialiased">
-                Check for your work and have nice Exam and quiz
-              </p>
-            </div>
-            <div className="p-4 pt-0">
-              <button
-                data-ripple-light="true"
-                type="button"
-                className="select-none rounded-lg bg-[#968BC9] py-2 px-4 text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              >
-                Read More
-              </button>
-            </div>
-          </div>
-          <div className="relative flex flex-col w-64 rounded-xl bg-white text-gray-700 shadow-md">
-            <div className="relative mx-4 -mt-6 h-32 overflow-hidden rounded-t-xl bg-blue-gray-500 text-white shadow-lg shadow-blue-gray-500/40">
-              <img src={Dashboard} />
-            </div>
-            <div className="p-4">
-              <h5 className="mb-2 block font-sans text-lg font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                Dashboard
-              </h5>
-              <p className="block font-sans text-sm font-light leading-relaxed text-inherit antialiased">
-                Easiest way to handle your daily tasks and Study stuff
-              </p>
-            </div>
-            <div className="p-4 pt-0">
-              <button
-                data-ripple-light="true"
-                type="button"
-                className="select-none rounded-lg bg-[#968BC9] py-2 px-4 text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              >
-                Read More
-              </button>
-            </div>
-          </div>
+          </section>
         </section>
-        <br /><br /><br />
 
 
       </main>
@@ -282,3 +273,82 @@ function Landing() {
 }
 
 export default Landing;
+
+
+{/* Service Section */ }
+{/* <section className="flex flex-col justify-center items-center flex-grow">
+          <h3 className="text-2xl font-semibold">OUR SERVICE</h3>
+          <img src={Service} className="w-120 justify-center" alt="Service" />
+        </section> */}
+
+
+{/* What makes us diffrent Part */ }
+{/* <section className="flex justify-center flex-grow gap-8">
+
+          <div className="relative flex flex-col w-64 rounded-xl bg-white text-gray-700 shadow-md">
+            <div className="relative mx-4 -mt-6 h-32 overflow-hidden rounded-t-xl bg-blue-gray-500 text-white shadow-lg shadow-blue-gray-500/40">
+              <img src={Courses} />
+            </div>
+            <div className="p-4">
+              <h5 className="mb-2 block font-sans text-lg font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                Courses
+              </h5>
+              <p className="block font-sans text-sm font-light leading-relaxed text-inherit antialiased">
+                Acess Hundreds of Courses for free and with paid bundles.
+              </p>
+            </div>
+            <div className="p-4 pt-0">
+              <button
+                data-ripple-light="true"
+                type="button"
+                className="select-none rounded-lg bg-[#968BC9] py-2 px-4 text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              >
+                Read More
+              </button>
+            </div>
+          </div>
+          <div className="relative flex flex-col w-64 rounded-xl bg-white text-gray-700 shadow-md">
+            <div className="relative mx-4 -mt-6 h-32 overflow-hidden rounded-t-xl bg-blue-gray-500 text-white shadow-lg shadow-blue-gray-500/40">
+              <img src={Exam} />
+            </div>
+            <div className="p-4">
+              <h5 className="mb-2 block font-sans text-lg font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                Exams & Quizzes
+              </h5>
+              <p className="block font-sans text-sm font-light leading-relaxed text-inherit antialiased">
+                Check for your work and have nice Exam and quiz
+              </p>
+            </div>
+            <div className="p-4 pt-0">
+              <button
+                data-ripple-light="true"
+                type="button"
+                className="select-none rounded-lg bg-[#968BC9] py-2 px-4 text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              >
+                Read More
+              </button>
+            </div>
+          </div>
+          <div className="relative flex flex-col w-64 rounded-xl bg-white text-gray-700 shadow-md">
+            <div className="relative mx-4 -mt-6 h-32 overflow-hidden rounded-t-xl bg-blue-gray-500 text-white shadow-lg shadow-blue-gray-500/40">
+              <img src={Dashboard} />
+            </div>
+            <div className="p-4">
+              <h5 className="mb-2 block font-sans text-lg font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                Dashboard
+              </h5>
+              <p className="block font-sans text-sm font-light leading-relaxed text-inherit antialiased">
+                Easiest way to handle your daily tasks and Study stuff
+              </p>
+            </div>
+            <div className="p-4 pt-0">
+              <button
+                data-ripple-light="true"
+                type="button"
+                className="select-none rounded-lg bg-[#968BC9] py-2 px-4 text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              >
+                Read More
+              </button>
+            </div>
+          </div>
+        </section> */}
