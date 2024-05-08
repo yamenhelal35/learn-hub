7c7b06d91486c36be651122cee0ba278a3422436
Ahmed Magdy <azzam9778@gmail.com>
2024-05-09 00:02:02 +0300

    LandingPage

--- FrontEnd/src/components/landingPage/landing.jsx
+++ FrontEnd/src/components/landingPage/landing.jsx
@@ -25,37 +25,32 @@ function Landing() {
     <div className="flex flex-col bg-white dark:bg-gray-900">
 
 
-      {/* Side Bar Fixed */}
+      {/* ======================== Side Bar Fixed================================= */}
       <Sidebar></Sidebar>
 
-
-      {/* Main Content */}
       <main className="flex-grow mt-8 ml-64 flex flex-col justify-center items-center">
-        <div className="absolute top-0 left-0 ml-72">
-          <SearchBar />
-        </div>
 
         {/* ======================Header Hero Section ================================ */}
         <header>
           <section class="bg-white dark:bg-gray-900">
-            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <div class="py-6 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
               <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-                <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Flowbite is out! See what's new</span>
+                <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">LearnHub Courses is out! See what's new</span>
                 <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
               </a>
-              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-              <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Empowering minds, transforming futures</h1>
+              <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Empower your learning journey with LearnHub – Seamlessly manage your assignments, exams, and study schedules all in one place. Connect with expert teachers who are dedicated to guiding you towards success.</p>
               <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                  Learn more
+                  Start Now
                   <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                   <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                  Watch video
+                  See Courses
                 </a>
               </div>
-              <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+              {/* <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
                 <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
                 <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
                   <a href="#" class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
@@ -87,20 +82,20 @@ function Landing() {
                     </svg>
                   </a>
                 </div>
-              </div>
+              </div> */}
             </div>
           </section>
         </header>
-        
+
 
 
         {/* =====================Service Parts ====================================== */}
         <section className="Service">
-          <section class="bg-white dark:bg-gray-900 flex justify-center items-center">
-            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
-              <div class="max-w-screen-md mb-8 lg:mb-16">
-                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-                <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <section class="bg-white dark:bg-gray-900 flex ">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-0 lg:px-6 ">
+              <div class="max-w-screen-md mb-8 lg:mb-16 ">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white ">Designed for anykind of eductaion needed</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-400">At LearnHub, we believe in making education accessible and enjoyable. Through innovative learning solutions and dedicated support, we empower learners worldwide to realize their dreams and make a positive impact on their communities.</p>
               </div>
               <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
                 <div>
