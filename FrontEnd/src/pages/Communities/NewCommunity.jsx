fa8c9e80f7a89b15291aaca5f33267090a5200c7
Ahmed Magdy <azzam9778@gmail.com>
2024-06-16 11:03:19 +0300

    Edit Login and Register Form

--- FrontEnd/src/pages/Communities/NewCommunity.jsx
+++ FrontEnd/src/pages/Communities/NewCommunity.jsx
@@ -20,11 +20,11 @@ const NewCommunity = () => {
 
                 <div className='flex flex-row'>
 
-
+                
                     <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none rounded-lg border flex flex-row border-gray-200  dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        <a href="CreateCommunity">Create Community</a>
+                        <a href="/CreateCommunity">Create Community</a>
                     </button>
-
+                   
                     <form class="flex items-center max-w-sm mx-auto ml-96">
                         <label for="simple-search" class="sr-only">Search</label>
                         <div class="relative w-full">
--- FrontEnd/src/pages/login/login.jsx
+++ FrontEnd/src/pages/login/login.jsx
@@ -2,43 +2,13 @@
 import jwt_decode from "jwt-decode";
 import React, { useState } from "react";
 import Text from "../login/Text.svg";
-
-import logo2 from "../login/login-promotion 1.png";
+import logo2 from "../../components/images/LoginNew.png";
 import { Link } from "react-router-dom";
 import { GoogleLogin } from '@react-oauth/google';
 import "./loginStyle.css";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-/*   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Basic validation
-    if (!username || !password) {
-      setError("Please enter both username and password");
-      return;
-    }
-
-    setError("");
-
-    // You can handle authentication logic here
-    console.log("Username:", username);
-    console.log("Password:", password);
-  };
- */
-
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -78,11 +48,11 @@ const Login = () => {
       }
 
       const loginData = await response.json(); // Parse response data
-      
-      if(loginData){
+
+      if (loginData) {
         document.cookie = `token=${loginData.token}; SameSite=Strict; Secure`;
         console.log("Login successful:", loginData);
-        navigate("/home");        
+        navigate("/home");
       }
 
     } catch (error) {
@@ -94,16 +64,16 @@ const Login = () => {
   return (
     <div className="w-full h-full flex items-start ">
       {/* Left Part Form and Login */}
-      <div className="w-1/2 h-screen flex flex-col p-20  justify-between bg-[#FAFAFA]">
-        <div className="w-full flex flex-col max-w-[550px]:">
-          <div className="w-full flex flex-col mb-1">
+      <div className="w-1/2 h-screen flex flex-col p-20  justify-between bg-gray-800">
+        {/* <div className="w-full flex flex-col max-w-[550px]:"> */}
+        {/* <div className="w-full flex flex-col mb-1">
             <h3 className="text-4xl font-bold mb-4">Login</h3>
             <p className="text-base mb-2">
               Welcome To LearnHub, Start your Journey Now
             </p>
-          </div>
+          </div> */}
 
-          {/* Form Register or Login */}
+        {/* Form Register or Login
           <div className="w-full flex flex-col">
             <input
               type="email"
@@ -150,17 +120,68 @@ const Login = () => {
                 Create new Account
               </button>
             </Link>
+          </div> */}
+        {/* </div> */}
+
+
+        <div class="w-full bg-white rounded-lg  md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+
+            <h1 className="text-sm font-semibold tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Welcome To LearnHub, Start your Journey Now
+            </h1>
+            {/* ========================Form of Credntails========================  */}
+            <form class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              {/* =======Email Input============================ */}
+                <input type="email" onChange={handleInputChange} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@LearnHub.com" required="" />
+              </div>
+              <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+               {/* =======Password Input============================ */}
+                <input type="password" onChange={handleInputChange} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                  </div>
+                </div>
+                {/* ==================Forget Password ================= */}
+                <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+              </div>
+              <button type="submit" onClick={handleSubmit} class="w-full  text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <Link to="/register">
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+
+                Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+              </p>
+              </Link>
+            </form>
           </div>
         </div>
       </div>
 
 
+
+
+
+
+
+
       {/* Right Part Logo and Button */}
-      <div className="w-1/2 h-screen flex flex-col p-10 justify-between">
-        <img src={Text} alt="" className="mb-3" />
+      <div className="w-1/2 h-screen flex flex-col p-10 justify-between bg-gray-800">
+        <img src={Text} alt="" className="mb-3 " />
         <img src={logo2} alt="" className="w-97" />
         <Link to="/register">
-          <button className="w-full bg-[#968BC9] rounded-md  p-4 text-center font-semibold text-white flex items-center justify-center">
+          <button className="w-full bg-primary-600 rounded-md  p-4 text-center font-semibold text-white flex items-center justify-center">
             Join Now For Free
           </button>
         </Link>
